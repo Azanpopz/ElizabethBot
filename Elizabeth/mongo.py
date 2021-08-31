e06cab2fb47358c0096cdeebdd47aba776dc2bb9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-31 22:21:35 +0530

    Update mongo.py

--- Elizabeth/mongo.py
+++ Elizabeth/mongo.py
@@ -3,22 +3,22 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from YoneRobot import DB_URI 
-from pymongo import MongoClient
+from Elizabeth import MONGO_DB_URI 
+from Elizabeth import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Elizabeth.conf import get_int_key, get_str_key
 
 
-DB_PORT = get_int_key("27017")
-DB_URI = get_str_key("DB_URI")
-DB = "DaisyX"
+MONGO_PORT = get_int_key("27017")
+MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
-client = MongoClient(DB_URI, DB_PORT)[DB]
-motor = motor_asyncio.AsyncIOMotorClient(DB_URI, DB_PORT)
-db = motor[DB]
-db = client["Elizabeth"]
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+db = motor[MONGO_DB]
+db = client["yonerobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
