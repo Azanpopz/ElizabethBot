0e97d003ada2a0c6988dc50f0af0b90d6e2ee45a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-31 22:24:31 +0530

    Update __init__.py

--- Elizabeth/__init__.py
+++ Elizabeth/__init__.py
@@ -173,6 +173,7 @@ else:
     CERT_PATH = Config.CERT_PATH
 
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    MONGO_DB_URI = Config.MONGO_DB_URI
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
