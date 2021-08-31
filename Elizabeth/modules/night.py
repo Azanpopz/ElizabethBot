1402037c0725c1b42b5231d417e5da56102f4f10
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-31 22:00:19 +0530

    Update night.py

--- Elizabeth/modules/night.py
+++ Elizabeth/modules/night.py
@@ -21,7 +21,7 @@ from telethon import *
 from telethon.tl.types import ChatBannedRights
 
 from Elizabeth.events import register
-from Elizabeth.mongo import db
+from Elizabeth.database import db
 from Elizabeth import client as tbot
 
 nightmod = db.nightmode
